d5a0de197785e8eac3f3e1077fe18b2b6b0e750c
Jan Müller <muller.jan@gmail.com>
2017-09-04 13:55:57 +0200

    Update default.php

--- conf/default.php
+++ conf/default.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * Default settings for the syntaxmantis plugin
+ *
+ * @author Jan Müller <muller.jan@gmail.com>
  */
 
 $conf['mantis_server']    = 'https://mantis.example.com/bugtracker';
